0b529b7c4db69f4baa387ce65a62869412124eb8
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-06-11 00:59:48 +0200

    feat: integrate omni-turtle-search with cubox service for enhanced search capabilities
    - Add cubox search client initialization in main.go
    - Implement search functionality in cubox.go to query cubox service with configurable parameters

--- app/main.go
+++ app/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/pkarpovich/omni-turtle-search/app/config"
+	"github.com/pkarpovich/omni-turtle-search/app/services/cubox"
+	"log"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	cfg, err := config.Init()
+	if err != nil {
+		log.Printf("[ERROR] Something went wrong: %v", err)
+	}
+
+	cuboxClient := cubox.NewClient(cfg.Cubox)
+
+	resp, err := cuboxClient.Search("golang")
+	if err != nil {
+		log.Printf("[ERROR] Something went wrong: %v", err)
+	}
+
+	log.Printf("[INFO] Response: %v", resp)
 }
---
+++ app/services/cubox/cubox.go
@@ -0,0 +1,63 @@
+package cubox
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pkarpovich/omni-turtle-search/app/config"
+	"io"
+	"net/http"
+)
+
+type Client struct {
+	config config.CuboxConfig
+}
+
+func NewClient(cfg config.CuboxConfig) *Client {
+	return &Client{
+		config: cfg,
+	}
+}
+
+type SearchResponse struct {
+	Items []SearchResponseItem `json:"data"`
+}
+
+type SearchResponseItem struct {
+	Id          string `json:"userSearchEngineID"`
+	Url         string `json:"targetURL"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+func (c *Client) Search(query string) ([]SearchResponseItem, error) {
+	httpClient := &http.Client{}
+
+	url := c.buildSearchURL(query)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", c.config.Token)
+
+	bodyResp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer bodyResp.Body.Close()
+
+	var searchResp SearchResponse
+	body, err := io.ReadAll(bodyResp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(body, &searchResp); err != nil {
+		return nil, err
+	}
+
+	return searchResp.Items, nil
+}
+
+func (c *Client) buildSearchURL(query string) string {
+	return fmt.Sprintf("https://cubox.cc/c/api/card/search?page=1&pageSize=50&keyword=%s&filters=&archiving=false", query)
+}
