d2374f69735eb6fc008044046a0814e48431c89d
Pavel Karpovich <pavel.karpovich@outlook.com>
2024-06-16 17:22:40 +0200

    feat(extension): enhance extension with background service worker and dynamic query search update
    - Add `service_worker` in manifest for background tasks
    - Implement message passing from background script on tab update to refresh query search in react hook dynamically

--- extension/src/hooks/useQuerySearch.ts
+++ extension/src/hooks/useQuerySearch.ts
@@ -1,6 +1,20 @@
-export const useQuerySearch = (): string => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const query = urlParams.get("q");
+import { useEffect, useState } from "react";
 
-    return query ?? "";
+const getQuerySearch = (): string => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get("q") ?? "";
+};
+
+export const useQuerySearch = (): string => {
+    const [query, setQuery] = useState<string>(getQuerySearch);
+
+    useEffect(() => {
+        chrome.runtime.onMessage.addListener((request) => {
+            if (request.message === "TabUpdated") {
+                setQuery(getQuerySearch);
+            }
+        });
+    }, []);
+
+    return query;
 };
